06545e213691f5442422e073f12b3e7c35388c98
findingDodi <dorina.diem@gmail.com>
2024-06-16 17:20:44 +0200

    assets and resources

--- app/src/main/java/de/hsd/modulearn/MainActivity.kt
+++ app/src/main/java/de/hsd/modulearn/MainActivity.kt
@@ -10,10 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -61,8 +69,6 @@ fun GreetingPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldExample() {
-    var presses by remember { mutableIntStateOf(0) }
-
     Scaffold(
         topBar = {
             TopAppBar(
@@ -71,28 +77,32 @@ fun ScaffoldExample() {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
-                    Text("Top app bar")
+                    Text("moduLearn")
                 }
             )
         },
         bottomBar = {
             BottomAppBar(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary,
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = "Bottom app bar",
-                )
-            }
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Home,
+                            contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.List,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Settings,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                }
+            )
         },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -106,8 +116,6 @@ fun ScaffoldExample() {
                     This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
 
                     It also contains some basic inner content, such as this text.
-
-                    You have pressed the floating action button $presses times.
                 """.trimIndent(),
             )
         }
--- app/src/main/java/de/hsd/modulearn/ui/theme/Type.kt
+++ app/src/main/java/de/hsd/modulearn/ui/theme/Type.kt
@@ -4,31 +4,108 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.unit.sp
 
+
+import de.hsd.modulearn.R
+
 // Set of Material typography styles to start with
+val RalewayFontFamily = FontFamily(
+    listOf(
+        Font(R.font.raleway_regular),
+        Font(R.font.raleway_light, FontWeight.Light),
+        Font(R.font.raleway_semibold, FontWeight.SemiBold),
+        Font(R.font.raleway_bold, FontWeight.Bold)
+    )
+)
+
+val RobotoFontFamily = FontFamily(
+    listOf(
+        Font(R.font.roboto_regular),
+        Font(R.font.roboto_light, FontWeight.Light),
+        Font(R.font.roboto_bold, FontWeight.Bold)
+    )
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+
+    displaySmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.sp,
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    headlineSmall = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 27.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 25.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 15.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 15.sp,
+        letterSpacing = 0.sp,
     )
-    */
 )
