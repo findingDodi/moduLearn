76287ae32dc123a1cf4ea7c18dfe4204565a13a3
findingDodi <dorina.diem@gmail.com>
2024-06-28 19:18:05 +0200

    refactoring

--- app/src/main/java/de/hsd/modulearn/utils/AppContext.kt
+++ app/src/main/java/de/hsd/modulearn/utils/AppContext.kt
@@ -1,7 +1,9 @@
 package de.hsd.modulearn.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 
+@SuppressLint("StaticFieldLeak")
 object AppContext {
     private lateinit var context: Context
 
@@ -13,6 +15,7 @@ object AppContext {
         if (!this::context.isInitialized) {
             throw IllegalStateException("Context is not initialized. Call initialize(context) first.")
         }
+
         return context
     }
 }
